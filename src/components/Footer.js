aae5ae129af21abb13fa5cdfc40897d1256859fa
CHATAING BEATRICE <beatrice.chataing@univ-lyon1.fr>
2025-03-31 17:28:55 +0200

    Mise en place du footer.

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+
+import '../styles/Footer.css'
+
+function Footer() {
+	
+
+	return (
+		<footer className='b2lp-footer'>
+			<div className='b2lp-footer-elem'>
+                Proudly powered by SIO 2
+			</div>
+			
+		</footer>
+	)
+}
+
+export default Footer
