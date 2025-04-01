3946a784f305927dae2e003f93183c25e85438bd
CHATAING BEATRICE <beatrice.chataing@univ-lyon1.fr>
2025-04-01 16:56:43 +0200

    Création du fichier de messages.3

---
+++ src/datas/posts.js
@@ -0,0 +1,81 @@
+export const posts = [
+	{
+        "id": 1,
+        "Date": "2024-06-11",
+        "Titre": "Et optio neque aut ut aut sit qui sit.",
+        "Contenu": "Quasi ea tempore et magni similique animi. Aliquam deserunt animi magni et quaerat. Dolore odio debitis sint dolore aut. Error omnis repudiandae aut laborum consectetur atque consequatur.",
+        "Commentaires": []
+    },
+    {
+        "id": 2,
+        "Date": "2024-06-10",
+        "Titre": "Quisquam et qui.",
+        "Contenu": "Non et est. Quia ut aut. Qui est et. Quisquam quisquam non.",
+        "Commentaires": []
+    },
+    {
+        "id": 3,
+        "Date": "2024-06-11",
+        "Titre": "Tempora qui deserunt consequatur quibusdam dolor.",
+        "Contenu": "Quibusdam consequatur autem velit fugit dolores deserunt. Temporibus quasi nobis et omnis necessitatibus. Iure sunt quidem sed repudiandae adipisci dolores atque.",
+        "Commentaires": [
+            {
+                "id": 1,
+                "Date": "2024-06-12",
+                "Auteur": "Carli Vandervort",
+                "Contenu": "Non temporibus et est. Non sint et quos dolor veniam. Id velit cum autem et facere. Iure aut ratione voluptatem ipsam ratione doloremque eos."
+            },
+            {
+                "id": 2,
+                "Date": "2024-06-12",
+                "Auteur": "Blanche Homenick DVM",
+                "Contenu": "Temporibus vero perferendis omnis laborum nobis illo hic. Repudiandae eligendi veniam sit soluta molestias reiciendis. Et dolores dolor aut esse. Rerum at minima nisi ut at voluptatum culpa."
+            },
+            {
+                "id": 3,
+                "Date": "2025-03-20",
+                "Auteur": "epidemais",
+                "Contenu": "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."
+            }
+        ]
+    },
+    {
+        "id": 4,
+        "Date": "2024-06-11",
+        "Titre": "Et eos facilis tenetur iure porro aut.",
+        "Contenu": "Iste ut non eos. Dignissimos et incidunt delectus non. Doloribus distinctio similique deleniti qui id.",
+        "Commentaires": [
+            {
+                "id": 1,
+                "Date": "2024-06-12",
+                "Auteur": "Blanche Homenick DVM",
+                "Contenu": "Aspernatur eum illum ut consequatur repellendus. Aliquid dolores sunt quo doloremque. Et totam doloremque temporibus voluptatem similique tempora odit."
+            },
+            {
+                "id": 2,
+                "Date": "2024-06-12",
+                "Auteur": "Carli Vandervort",
+                "Contenu": "Molestiae voluptatibus repudiandae rerum consequatur nemo quas sed consectetur. Qui alias facilis iure dolorem eum et quod. Repudiandae omnis fuga eveniet voluptatem sit aspernatur."
+            },
+            {
+                "id": 3,
+                "Date": "2024-06-12",
+                "Auteur": "Carli Vandervort",
+                "Contenu": "Libero illum nihil ab omnis. Maiores omnis nesciunt ea nostrum quia facilis quos. Est nihil delectus quae aliquid qui quo est quo."
+            },
+            {
+                "id": 4,
+                "Date": "2024-06-12",
+                "Auteur": "Blanche Homenick DVM",
+                "Contenu": "Quisquam et qui veniam omnis totam est. Sed aut quas laboriosam sint dolorem quis. Nisi excepturi esse quisquam quod. Omnis autem aperiam eum non perspiciatis."
+            }
+        ]
+    },
+    {
+        "id": 5,
+        "Date": "2024-06-11",
+        "Titre": "Quia et ad aliquid cumque.",
+        "Contenu": "Autem veritatis totam nisi aliquid itaque. Et voluptate et amet in voluptates. Tempore distinctio cum eveniet tempora dicta quos.",
+        "Commentaires": []
+    }
+]
