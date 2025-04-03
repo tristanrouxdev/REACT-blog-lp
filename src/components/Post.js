fd17706b306d378a095575c7d1fc12bb9ac6d835
CHATAING BEATRICE <beatrice.chataing@univ-lyon1.fr>
2025-04-03 19:57:38 +0200

    Ajout d'un lien vers les commentaires s'ils existent avec style.

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,9 +1,15 @@
-function Post({  date, title, content } ) {
+import '../styles/Post.css'
+
+function Post({  date, title, content, comments } ) {
+    const hasComments = comments && Object.keys(comments).length > 0;
   return (
     <li>
       <h2>{title}</h2>
       <p>{date}</p>
       <p>{content}</p>
+      {hasComments ? (
+        <a href="/" className="post-link">voir les commentaires.</a>
+      ) : null }
     </li>
   );
 }
