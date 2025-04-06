59cba4d989a920bc3ca870377c6ab95726e703ea
CHATAING BEATRICE <beatrice.chataing@univ-lyon1.fr>
2025-04-06 16:11:07 +0200

    Commentaires implémentés et fonctionnels.

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -2,9 +2,9 @@
 function Comment({ date, author, comment }) {
   return (
     <li>
-        <p>{comment.date}</p>
-        <span>{comment.author}</span>
-        <p>{comment.content}</p>
+        <p>{date}</p>
+        <span>{author}</span>
+        <p>{comment}</p>
     </li>
   );
 }
--- src/components/Post.js
+++ src/components/Post.js
@@ -2,19 +2,37 @@ import '../styles/Post.css'
 import { useState } from 'react';
 import Comment from './Comment'
 
-function Post({  date, title, content, comments } ) {
-    const [showComments, setShowComments] = useState(false);
-    const hasComments = comments && Object.keys(comments).length > 0;
+function Post({ date, title, content, comments }) {
+  const [showComments, setShowComments] = useState(false); // État pour gérer la visibilité des commentaires
+  const hasComments = comments && Object.keys(comments).length > 0;
+
+  const toggleComments = (e) => {
+    e.preventDefault(); // Empêche le rechargement de la page
+    setShowComments(!showComments); // Inverse l'état de visibilité
+  };
+
   return (
     <li>
       <h2>{title}</h2>
       <p>{date}</p>
       <p>{content}</p>
-      {hasComments ? (
-        <a href="/" className="post-link">voir les commentaires.</a>
-      ) : null }
+      {hasComments && (
+        <a href="/" className="post-link" onClick={toggleComments}>
+          {showComments ? 'Masquer les commentaires' : 'Voir les commentaires'}
+        </a>
+      )}
+      {showComments && hasComments && (
+        <div className="post-comments">
+          <h3>Commentaires</h3>
+          <ul>
+            {Object.entries(comments).map(([id, { date, author, content }]) => (
+              <Comment key={id} date={date} author={author} content={content} />
+            ))}
+          </ul>
+        </div>
+      )}
     </li>
   );
 }
 
-export default Post
+export default Post;
